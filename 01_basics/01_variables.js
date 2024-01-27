c69f9ccaffff15100fb31278baf78565fb37640b
aalokydv <157775848+aalokydv@users.noreply.github.com>
2024-01-27 06:52:20 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 20078
+let accountEmail ="aalok987@gmail.com"
+var accountPassword ="12345"
+accountCity = "bhiwani"
+let accountState
+
+accountEmail = "jgfjdhfv.com"
+accountPassword= "654745"
+accountCity = "narnaul"
+
+// prefer not to use var
+// because of issue in block acope and functional scope
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3)  //we r using node js not browser
+
+console.log("aalok");
+
+
+//datatypes
+let name = "aalok"
+let age = 22
+let isLoggedin = true
+
+//number =>2  to power 53
+//bigint used for large number
+//string =>" "
+//boolean => true/false
+//null => standalone value
+//undefined => value is not assinged 
+//symbol => uniqueness
+
+
+//object
+
+
+console.log(typeof undefined );  //undefined 
+console.log(typeof null);  //output says objet is the datatype of null
+
